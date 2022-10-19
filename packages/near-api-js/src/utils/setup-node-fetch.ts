33b04459731df5f2785b69c28f775bca8fd92778
Adrian Serafin <adrian@softmad.pl>
2022-10-19 14:12:39 +0200

    add cacheable lookups, add build files to the repo

--- packages/near-api-js/src/utils/setup-node-fetch.ts
+++ packages/near-api-js/src/utils/setup-node-fetch.ts
@@ -1,9 +1,15 @@
 import fetch from 'node-fetch';
 import http from 'http';
 import https from 'https';
+import CacheableLookup from "cacheable-lookup";
+
+const lookup = new CacheableLookup();
 
 const httpAgent = new http.Agent({ keepAlive: true });
 const httpsAgent = new https.Agent({ keepAlive: true });
+
+lookup.install(httpAgent);
+lookup.install(httpsAgent);
 
 function agent(_parsedURL) {
     if (_parsedURL.protocol === 'http:') {
